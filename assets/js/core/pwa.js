48a31795e2a2d56cf2b6b3f61c76041acf43818d
arthurwhit3 <arthurmartin634@gmail.com>
2026-04-24 00:50:31 -0300

    weps

--- assets/js/core/pwa.js
+++ assets/js/core/pwa.js
@@ -4,8 +4,21 @@
   }
 
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js").catch((erro) => {
-      console.warn("Falha ao registrar service worker:", erro);
-    });
+    navigator.serviceWorker
+      .register("/service-worker.js", { updateViaCache: "none" })
+      .then((registration) => {
+        registration.update().catch(() => {});
+
+        navigator.serviceWorker.addEventListener("controllerchange", () => {
+          window.location.reload();
+        });
+
+        if (registration.waiting) {
+          registration.waiting.postMessage({ type: "SKIP_WAITING" });
+        }
+      })
+      .catch((erro) => {
+        console.warn("Falha ao registrar service worker:", erro);
+      });
   });
 })(window);
